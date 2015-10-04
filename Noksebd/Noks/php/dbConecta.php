a0c65b89c639c7a62899d53f0dafb41b24d3d17b
Vitor Hugo Lacerda Lana <vitorhugo.lanna@gmail.com>
2015-10-04 16:50:50 -0300

    Update dbConecta.php

--- Noksebd/Noks/php/dbConecta.php
+++ Noksebd/Noks/php/dbConecta.php
@@ -1,4 +1,5 @@
 <?php
-    mysql_connect("localhost", "root", ""); // Conecta com o MySQL - IP, usuário, senha
+    $conexao = mysql_connect("localhost", "root", ""); // Conecta com o MySQL - IP, usuário, senha
     mysql_select_db("noks"); // Conecta com o Banco - nome do banco
+    mysql_set_charset('UFT8', $conexao);
 ?>
